b6626aeb967131dbabd3ad64848999a3dd1ded92
stpngyun1902 <nxn190023>
2020-11-25 21:41:28 -0600

    Make changes

---
+++ printFunction.cc
@@ -0,0 +1,19 @@
+/*
+ * Filename: printFunction.cc
+ * Date: November 25, 2020
+ * Author: Anh Nguyen
+ * Email: nxn190023@utdallas.edu
+ * Course CS 3377.002 Fall 2020
+ * Version 1.0 (or correct version)
+ * Copyright 2020, All Rights Reserved
+ */
+
+#include <iostream>
+using namespace std;
+void printMessage()
+{
+  //just an intro to my Matrix! :)
+
+  cout << "Hello! I am a matrix created using curses and CDK!" << endl;
+
+}
